2839fe36a5b23b671fb5a72559606ad61e5b5706
everjs78 <everjs@gmail.com>
2020-07-07 17:48:26 +0900

    [Header] add header links and login buttons

--- src/components/basic/Body.vue
+++ src/components/basic/Body.vue
@@ -1,5 +1,26 @@
 <template>
   <div>
+    <v-navigation-drawer v-model="drawer" app clipped>
+      <v-list dense>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-view-dashboard</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Dashboard</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-cog</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Settings</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
     <v-main>
       <v-container class="fill-height" fluid>
         <v-row align="center" justify="center">
--- src/components/basic/Header.vue
+++ src/components/basic/Header.vue
@@ -1,29 +1,32 @@
 <template>
   <div>
-    <v-navigation-drawer v-model="drawer" app clipped>
-      <v-list dense>
-        <v-list-item link>
-          <v-list-item-action>
-            <v-icon>mdi-view-dashboard</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Dashboard</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-        <v-list-item link>
-          <v-list-item-action>
-            <v-icon>mdi-cog</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Settings</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-
     <v-app-bar app clipped-left>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-      <v-toolbar-title>Application</v-toolbar-title>
+      <!--<v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>-->
+      <v-btn icon class="ml-8">
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+      <v-toolbar-title>Hub</v-toolbar-title>
+
+      <!-- MenuLink 
+      <v-row justify="center" no-gutters>-->
+      <v-row class="ml-8">
+        <v-btn
+          v-for="link in links"
+          :key="link"
+          color="black"
+          text
+          rounded
+          class="my-2"
+        >
+          {{ link }}
+        </v-btn>
+      </v-row>
+      <v-spacer></v-spacer>
+
+      <v-btn small class="mx-2" color="primary" outlined width="100px"
+        >Login</v-btn
+      >
+      <v-btn small class="mx-2" color="primary" width="100px">Signup</v-btn>
     </v-app-bar>
   </div>
 </template>
@@ -34,8 +37,10 @@ export default {
   props: {
     source: String
   },
+
   data: () => ({
-    drawer: null
+    drawer: null,
+    links: ['Docuemnt', 'Pricing', 'Support']
   }),
   created() {
     this.$vuetify.theme.dark = false;
