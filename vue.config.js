f0f063d4b46d0df9b7145c0824615236bc10d72c
everjs78 <everjs@gmail.com>
2020-10-14 14:29:33 +0900

    [route] fix error that doesn't go to sub routes(project/projects) when refreshing - set public path to '/'

--- vue.config.js
+++ vue.config.js
@@ -2,29 +2,12 @@ const appManifestPlugin = require('./src/appManifestPlugin');
 const webpack = require('webpack');
 const publicPath = '/';
 
+// get version from package.json
+const fs = require('fs');
+const packageJson = fs.readFileSync('./package.json');
+const version = JSON.parse(packageJson).version || 0;
+
 module.exports = {
-  publicPath,
-  devServer: {
-    proxy: {
-      '/ui/v1': {
-        target: 'https://localhost:8000',
-      },
-    },
-  },
-
-  configureWebpack: {
-    devtool: 'source-map',
-    resolve: {
-      alias: {
-        vue$: require('path').resolve(__dirname, './node_modules/vue/dist/vue.runtime.esm.js'),
-      },
-    },
-    plugins: [appManifestPlugin(publicPath), new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/)],
-  },
-
-  chainWebpack: (config) => config.resolve.symlinks(false),
-  transpileDependencies: ['vuetify'],
-
   pluginOptions: {
     i18n: {
       locale: 'en',
@@ -33,4 +16,32 @@ module.exports = {
       enableInSFC: true,
     },
   },
+  publicPath,
+  devServer: {
+    proxy: {
+      '/ui/v1': {
+        target: 'https://localhost:8000',
+      },
+    },
+  },
+  configureWebpack: {
+    devtool: 'source-map',
+    resolve: {
+      alias: {
+        vue$: require('path').resolve(__dirname, './node_modules/vue/dist/vue.runtime.esm.js'),
+      },
+    },
+    plugins: [
+      appManifestPlugin(publicPath),
+      new webpack.DefinePlugin({
+        'process.env': {
+          PACKAGE_VERSION: '"' + version + '"',
+        },
+      }),
+      new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+    ],
+  },
+
+  // chainWebpack: (config) => config.resolve.symlinks(false),
+  transpileDependencies: ['vuetify'],
 };
