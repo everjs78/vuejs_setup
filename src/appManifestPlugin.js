f56f125c42b3f25128d8513e864a59e7885e6b23
everjs78 <everjs@gmail.com>
2020-10-14 14:36:32 +0900

    [route] fix error of AppManifest plugin that occured since error refreshing subpath

--- src/appManifestPlugin.js
+++ src/appManifestPlugin.js
@@ -2,18 +2,20 @@ const AppManifestWebpackPlugin = require('app-manifest-webpack-plugin');
 const path = require('path');
 
 module.exports = function appManifestPlugin(publicPath) {
+  if (publicPath === '/') {
+    publicPath = '';
+  }
+
   return new AppManifestWebpackPlugin({
     logo: path.resolve(__dirname, './assets/favicon.svg'),
-    prefix:
-      publicPath + (process.env.NODE_ENV === 'production' ? '/' : '/icons'),
-    output:
-      process.env.NODE_ENV === 'production' ? '/icons-[hash:8]/' : './icons/',
+    prefix: publicPath + (process.env.NODE_ENV === 'production' ? '/' : '/icons'),
+    output: process.env.NODE_ENV === 'production' ? '/icons-[hash:8]/' : './icons/',
     config: {
       appName: 'Blocko Hub Manager',
       icons: {
         yandex: false,
-        coast: false
-      }
-    }
+        coast: false,
+      },
+    },
   });
 };
