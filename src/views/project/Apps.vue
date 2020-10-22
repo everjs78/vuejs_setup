07e6b3b3ea337eb2ffa286c6648aaa421c048d35
everjs78 <everjs@gmail.com>
2020-10-22 11:28:22 +0900

    [project] apply classifictaion of apps in project detail component

--- src/views/project/Apps.vue
+++ src/views/project/Apps.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-    <v-row align="center" justify="center" v-for="section in appSections" :key="section" class="section pt-2 pb-4">
-      <v-col>
+    <v-row align="center" justify="center" v-for="section in appSections" :key="section.name" class="section pt-2 pb-4">
+      <v-col v-if="section.apps !== undefined && section.apps.length > 0">
         <v-row class="title" cols="12">
-          <v-col class="pb-0">{{ section }}</v-col>
+          <v-col class="pb-0">{{ section.name }}</v-col>
         </v-row>
-        <v-row v-for="app in apps" :key="app.name">
+        <v-row v-for="app in section.apps" :key="app.name">
           <v-col class="app-card" cols="12">
             <AppCard :app="app" />
           </v-col>
@@ -33,8 +33,20 @@ export default Vue.extend({
   },
   data() {
     return {
-      appSections: ['Web', 'Workers', 'ManagedApps'],
-      apps: [],
+      appSections: [
+        {
+          name: 'Web',
+          apps: [],
+        },
+        {
+          name: 'Workers',
+          apps: [],
+        },
+        {
+          name: 'ManagedApps',
+          apps: [],
+        },
+      ],
     };
   },
   methods: {
@@ -42,7 +54,9 @@ export default Vue.extend({
       const data = await this.$rest.get(`/ui/v1/projects/${this.projectName}`);
       if (data) {
         console.log('projectinfo:', data);
-        this.apps = data.apps;
+        this.appSections[0].apps = data.apps['webs'];
+        this.appSections[1].apps = data.apps['workers'];
+        this.appSections[2].apps = data.apps['manages-apps'];
       }
     },
     goApp(appName: string) {
